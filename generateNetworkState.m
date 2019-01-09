b4db8cd15ba6a26c29ba83b149e535b08da41a25
jgpc <juangpc@gmail.com>
2019-01-09 19:23:56 +0100

    previous to simulation 0

--- generateNetworkState.m
+++ generateNetworkState.m
@@ -1,9 +1,8 @@
 function S = generateNetworkState(n,Sth)
 
-S=[rand(1,n)*2*Sth;
-  zeros(1,n);
-  zeros(1,n)]';
-    
+S=[rand(1,n)*2*Sth; %internal state potential
+  zeros(1,n); % global time for las update
+  zeros(1,n)]'; %refractory state flag   
 end
 
 
--- model3.m
+++ model3.m
@@ -11,6 +11,9 @@ b=0; %ttf defines S max.
 v=5; %spike transmission speed (m/s)
 D=0.05; %decay constant
 t=0; %global time
+tarp=2; % absolute refractory period
+
+maxSimTime=4000;
 
 %build network
 [W,Dlay,A]=generateNetworkConnections(n);
@@ -36,8 +39,8 @@ clear new_event
 
 % main iteration
 evlog=[];
-while(~isempty(evlist))
-  if(evlist(1,1)==1) %firing spike
+while(~isempty(evlist) && (evlist(1,3)<maxSimTime))
+  if(evlist(1,1)==1) %firing spike event
     jneurons=find_connections(evlist(1,2),W);
     for j=1:length(jneurons)
       new_event=[2 jneurons(j) ...
@@ -47,47 +50,53 @@ while(~isempty(evlist))
       evlist=cat(1,evlist,new_event);
       evlist=sortrows(evlist,3);
     end
-    S(evlist(1,2),1)=0; %new state = 0
+    S(evlist(1,2),1)=0; %new state = 0 just after firing
     S(evlist(1,2),2)=evlist(1,3); %time of update
     S(evlist(1,2),3)=1; %refractory state flag ON
-    new_event=[3 evlist(1,2) 0 0];
+    new_event=[3 evlist(1,2) evlist(1,3)+tarp 0];
     evlist=cat(1,evlist,new_event);
     evlist=sortrows(evlist,3);
     
-  elseif(evlist(1,1)==2) %burning spike (spike reception)
-    if(S(evlist(1,2),1)>=Sth) %neuron in active state
-      dT=(evlist(1,3)-S(evlist(1,2),2));
-      Tr=(S(evlist(1,2),1)-1)^2*dT/(a-(S(evlist(1,2),1)-1)*dT);
-      S(evlist(1,2),1)=S(evlist(1,2),1)+evlist(1,4)+Tr;
-      S(evlist(1,2),2)=evlist(1,3);
-      
-      ev_firing=find(evlist(:,1)==1);
-      expired_event=ev_firing(find(evlist(ev_firing,2)==evlist(1,2)));
-      evlist(expired_event,:)=[];
-      
-      ttf=(a/(S(evlist(1,2),1)-1))-b;
-      new_event=[1 evlist(1,2) S(evlist(1,2),2)+ttf 0];
-      evlist=cat(1,evlist,new_event);
-      evlist=sortrows(evlist,3);
-      
-      if(S(evlist(1,2),1)<Sth) %active to passive transition
-        ev_firing=find(evlist(:,1)==1);
-        expired_event=ev_firing(find(evlist(ev_firing,2)==evlist(1,2)));
-        evlist(expired_event,:)=[];
-      end
-    else %neuron in passive state
-      dT=(evlist(1,3)-S(evlist(1,2),2));
-      S(evlist(1,2),1)=S(evlist(1,2),1)*exp(-dT/D)+evlist(1,4);
-      S(evlist(1,2),2)=evlist(1,3);
-      if(S(evlist(1,2),1)>=Sth) %passive to active transition
-        ttf=(a/(S(evlist(1,2),1)-1))-b;
-        new_event=[1 evlist(1,2) evlist(1,3)+ttf 0];
+  elseif(evlist(1,1)==2) %burning spike (spike reception) event
+    if(S(evlist(1,2),3)==0) %only if not in refractory state
+      if(S(evlist(1,2),1)>=Sth) %neuron in active state
+        dT=(evlist(1,3)-S(evlist(1,2),2));
+        Tr=(S(evlist(1,2),1)-1)^2*dT/(a-(S(evlist(1,2),1)-1)*dT);
+        S(evlist(1,2),1)=S(evlist(1,2),1)+evlist(1,4)+Tr;
+        S(evlist(1,2),2)=evlist(1,3);
+        
+        if(S(evlist(1,2),1)>=Sth) %continues in active state
+          %erase previous firing event
+          ev_firing=find(evlist(:,1)==1);
+          expired_event=ev_firing(find(evlist(ev_firing,2)==evlist(1,2)));
+          evlist(expired_event,:)=[];
+
+          %generate new updated firing event
+          ttf=(a/(S(evlist(1,2),1)-1))-b;
+          new_event=[1 evlist(1,2) S(evlist(1,2),2)+ttf 0];
+          evlist=cat(1,evlist,new_event);
+          evlist=sortrows(evlist,3);
+        
+        elseif(S(evlist(1,2),1)<Sth) %active to passive transition
+          ev_firing=find(evlist(:,1)==1);
+          expired_event=ev_firing(find(evlist(ev_firing,2)==evlist(1,2)));
+          evlist(expired_event,:)=[];
+        end
+      else %neuron in passive state
+        dT=(evlist(1,3)-S(evlist(1,2),2));
+        S(evlist(1,2),1)=S(evlist(1,2),1)*exp(-dT/D)+evlist(1,4);
+        S(evlist(1,2),2)=evlist(1,3);
+        
+        if(S(evlist(1,2),1)>=Sth) %passive to active transition
+          ttf=(a/(S(evlist(1,2),1)-1))-b;
+          new_event=[1 evlist(1,2) evlist(1,3)+ttf 0];
+        end
       end
     end
     
   elseif(evlist(1,1)==3) %refractory state
     S(evlist(1,2),3)=0;
-    S(evlist(1,2),2)=evlist(1,3);
+    %S(evlist(1,2),2)=evlist(1,3);
   end
   evlog=cat(1,evlog,evlist(1,:));
   evlist(1,:)=[];
